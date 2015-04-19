81f10caa94d64afb9b876520aa2b2337e5876302
Wojtek Janoszek <wetcouch46wojtek@gmail.com>
2015-04-19 20:23:36 +0200

    First version, mostly HTML and CSS

---
+++ script.js
@@ -0,0 +1,36 @@
+/**
+ * Created by wetcouch on 19.04.2015.
+ */
+var score = 0;
+var currentPage = 0;
+
+var questions = [{
+    question: "Is that true, that you can kill basilisk with a mirror?",
+    choices: ["Yes", 'No', 'Yes, if you hit him in the head'],
+    correctAnswer: 2
+}, {
+    question: "Nobody expect...?",
+    choices: ["Mutated dog", 'Spanish Inquisition', 'Me'],
+    correctAnswer: 1
+}, {
+    question: 'Easy peasy?',
+    choices: ['Lemon Squeezy', 'Yes', 'Whaa..?'],
+    correctAnswer: 0
+}];
+
+function nextPage () {
+    page = currentPage + 1;
+    function allSels (num) {
+        $("#sel" + num).html("<input type='radio' name='1'/>" + questions[page].choices[num])
+    }
+    $("#question").text(questions[page].question);
+    allSels(0);
+    allSels(1);
+    allSels(2);
+}
+$(document).ready(function () {
+    $(".next").click(function changePage() {
+        nextPage();
+        currentPage += 1;
+    });
+});
